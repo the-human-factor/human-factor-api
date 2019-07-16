9bfd389946ed271d3feed61836374d98acccc327
Leo Urbina <leo@cloudflare.com>
2019-07-16 09:55:18 -0500

    Fixes CORS and schemas

--- api/schemas.py
+++ api/schemas.py
@@ -6,6 +6,7 @@ import api.models as models
 class VideoSchema(ModelSchema):
   class Meta:
     model = models.Video
+    exclude = ('response', 'challenges')
 
 class UserSchema(ModelSchema):
   class Meta:
@@ -22,6 +23,14 @@ class ResponseSchema(ModelSchema):
   class Meta:
     model = models.Response
 
-  challenge = fields.Nested(ChallengeSchema)
+  class ChallengeWithoutResponses(ModelSchema):
+    class Meta:
+      model = models.Challenge
+      exclude = ('responses',)
+
+    video = fields.Nested(VideoSchema)
+    user = fields.Nested(UserSchema)
+
+  challenge = fields.Nested(ChallengeWithoutResponses)
   video = fields.Nested(VideoSchema)
   user = fields.Nested(UserSchema)
