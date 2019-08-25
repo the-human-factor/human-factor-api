04b0b904adb1dbf571fc683f8e6463550b458219
Leo Urbina <leo.a.urbina@gmail.com>
2019-08-25 00:40:17 -0700

    Adds helm chart, fix env

--- api/tests/test_video.py
+++ api/tests/test_video.py
@@ -19,3 +19,10 @@ def test_get_video(client, access_token):
 
   assert resp.status_code == 200
   assert resp.json['id'] == str(video.id)
+
+def test_get_video_without_token(client):
+  video = f.VideoFactory.create().save()
+
+  resp = client.get(url_for('video', video_id=video.id))
+
+  assert resp.status_code == 401
